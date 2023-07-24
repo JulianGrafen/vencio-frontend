3cff65320c1399f3533620c54dccfd7fd81d1887
JulianGrafen <juliangraefen@gmail.com>
2023-07-24 17:45:39 +0200

    changed website text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div>Vencio.eu</div>;
+  return (
+    <div>
+      vencio.eu is a property of Henri Grüger and Julian Gräfen. For business
+      related questions mail us at: projekt.vencio@gmail.com
+    </div>
+  );
 }
 
 export default App;
